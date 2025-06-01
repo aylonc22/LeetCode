2e754c010732a48ea673a014650e59c0241d8fe4
Aylon <aylonc1@gmail.com>
2025-06-01 14:59:38 +0300

    5

---
+++ src/5.js
@@ -0,0 +1,29 @@
+if (s.length < 2) return s;
+
+    let start = 0, end = 0;
+
+    const expandAroundCenter = (left, right) => {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return [left + 1, right - 1];
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        // Odd length palindrome
+        let [left1, right1] = expandAroundCenter(i, i);
+        // Even length palindrome
+        let [left2, right2] = expandAroundCenter(i, i + 1);
+
+        if (right1 - left1 > end - start) {
+            start = left1;
+            end = right1;
+        }
+        if (right2 - left2 > end - start) {
+            start = left2;
+            end = right2;
+        }
+    }
+
+    return s.substring(start, end + 1);
